d26f93d4b209f77e6556d6fe339c39cbc22ef06e
David Eccles (gringer) <bioinformatics@gringene.org>
2015-11-21 06:27:10 +1000

    modify longest base filter to output SAM (selectable by hard-coded variable)

--- sam2LongestBase.pl
+++ sam2LongestBase.pl
@@ -6,6 +6,9 @@ my $bestFlags = "";
 my $bestID = "";
 my $bestSeq = "";
 my $bestQual = "";
+my $bestLine = "";
+
+my $output = "sam"; # can be "sam" or "fastq"
 
 sub printSeq {
     my ($id, $seq, $qual) = @_;
@@ -14,19 +17,34 @@ sub printSeq {
     }
 }
 
+
 while(<>){
+    if(/^@/){
+	print;
+	next;
+    }
+    my $line = $_;
     chomp;
     my @F = split(/\t/);
     if(($F[2] ne $seqName) || ($F[3] != $pos) || (length($bestSeq) < length($F[9]))){
 	if(($F[2] ne $seqName) || ($F[3] != $pos)){
-	    printSeq($bestID, $bestSeq, $bestQual);
+	    if($output eq "fastq"){
+		printSeq($bestID, $bestSeq, $bestQual);
+	    } elsif($output eq "sam"){
+		print($bestLine);
+	    }
 	}
 	$seqName = $F[2];
 	$pos = $F[3];
+	$bestLine = $line;
 	$bestID = $F[0];
 	$bestFlags = $F[1];
 	$bestSeq = $F[9];
 	$bestQual = $F[10];
     }
 }
-printSeq($bestID, $bestSeq, $bestQual);
+if($output eq "fastq"){
+    printSeq($bestID, $bestSeq, $bestQual);
+} elsif($output eq "sam"){
+    print($bestLine);
+}
